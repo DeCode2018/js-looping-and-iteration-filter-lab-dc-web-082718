4c319abebf35f38fa9e40b265ab0a78945b00aa3
DeCode2018 <decode2018@gmail.com>
2018-12-06 17:39:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers,string){
+  return drivers.filter(driver => driver.toLowerCase().includes(string.toLowerCase()))
+}
+
+function fuzzyMatch(drivers,string){
+  return drivers.filter(driver => driver.slice(0,2)=== string)
+}
+
+function matchName(drivers,string){
+  return drivers.filter(driver => driver.name.includes(string))
+}
